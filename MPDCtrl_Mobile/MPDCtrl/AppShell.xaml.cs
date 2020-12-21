c3c8308ee0afd80b5ec38bd522c6c2d05dc6e4cd
torum <37834607+torum@users.noreply.github.com>
2020-12-21 09:10:39 +0900

    Added Mobile Template

---
+++ MPDCtrl_Mobile/MPDCtrl/AppShell.xaml.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using MPDCtrl.ViewModels;
+using MPDCtrl.Views;
+using Xamarin.Forms;
+
+namespace MPDCtrl
+{
+    public partial class AppShell : Xamarin.Forms.Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+            Routing.RegisterRoute(nameof(ItemDetailPage), typeof(ItemDetailPage));
+            Routing.RegisterRoute(nameof(NewItemPage), typeof(NewItemPage));
+        }
+
+    }
+}

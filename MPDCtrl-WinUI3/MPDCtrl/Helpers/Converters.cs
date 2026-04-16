a6301e77fe23c7c35864b641fee828db2052b110
torum <37834607+torum@users.noreply.github.com>
2026-04-17 05:12:44 +0900

    Make NowPlaying text SemiBold using converter

---
+++ MPDCtrl-WinUI3/MPDCtrl/Helpers/Converters.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.UI.Text;
+using Microsoft.UI.Xaml.Data;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MPDCtrl.Helpers
+{
+    public partial class BoolToFontWeightConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, string language)
+        {
+            if (value is bool isBold && isBold)
+            {
+                return FontWeights.SemiBold;
+            }
+            return FontWeights.Normal;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, string language)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}

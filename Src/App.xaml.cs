c7a925fb6fe31e0e016f99c0f5d2419fa6f11e77
torum <37834607+torum@users.noreply.github.com>
2026-05-04 11:40:10 +0900

    merge

--- Src/App.xaml.cs
+++ Src/App.xaml.cs
@@ -57,7 +57,7 @@ public partial class App : Application
     public static T GetService<T>()
         where T : class
     {
-        if ((App.Current as App)!.Host.Services.GetService(typeof(T)) is not T service)
+        if ((Application.Current as App)!.Host.Services.GetService(typeof(T)) is not T service)
         {
             throw new ArgumentException($"{typeof(T)} needs to be registered in ConfigureServices within App.xaml.cs.");
         }

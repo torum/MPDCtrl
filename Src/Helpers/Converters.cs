1bd7bab177cd37047f238187ded59e68f91c43e9
torum <37834607+torum@users.noreply.github.com>
2026-09-09 06:00:19 +0900

    Added TimeIntToStringValueConverter to format slider's tooltip value.

--- Src/Helpers/Converters.cs
+++ Src/Helpers/Converters.cs
@@ -20,4 +20,28 @@ namespace MPDCtrl.Helpers
             throw new NotImplementedException();
         }
     }
+
+    public sealed partial class TimeIntToStringValueConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, string language)
+        {
+            if (value is double sec)
+            {
+                int min, hour, s;
+
+                min = (int)(sec / 60);
+                s = (int)(sec % 60);
+                hour = min / 60;
+                min %= 60;
+
+                return $"{hour}:{min:00}:{s:00}";
+            }
+            return value?.ToString() ?? "0:00:00";
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, string language)
+        {
+            throw new NotImplementedException(); // Not needed for slider tooltips
+        }
+    }
 }
